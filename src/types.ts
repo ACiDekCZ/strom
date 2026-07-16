3bd7d841e419f129fb83334cf9d95737f4ba5a31
ACiDekCZ <milan.visek@gmail.com>
2026-07-16 17:49:15 +0200

    Release 1.18.0

--- src/types.ts
+++ src/types.ts
@@ -482,7 +482,7 @@ export interface AuditLog {
  * should be kept in sync with package.json.
  */
 declare const __APP_VERSION__: string | undefined;
-export const APP_VERSION = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : '1.17.1';
+export const APP_VERSION = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : '1.18.0';
 
 /** Envelope wrapping embedded data in exported HTML files */
 export interface EmbeddedDataEnvelope {
