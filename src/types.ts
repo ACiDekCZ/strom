6678a2ac129eef581cd18dad63577d74ad70f884
ACiDekCZ <milan.visek@gmail.com>
2026-09-24 19:21:18 +0200

    Beta 3.1.0-beta.4

--- src/types.ts
+++ src/types.ts
@@ -598,7 +598,7 @@ export interface AuditLog {
  * should be kept in sync with package.json.
  */
 declare const __APP_VERSION__: string | undefined;
-export const APP_VERSION = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : '3.1.0-beta.3';
+export const APP_VERSION = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : '3.1.0-beta.4';
 
 /** Envelope wrapping embedded data in exported HTML files */
 export interface EmbeddedDataEnvelope {
